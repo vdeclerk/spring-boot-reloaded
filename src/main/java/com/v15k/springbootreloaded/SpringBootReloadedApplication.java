65bb7140096900f7f8916e8c6244402f3f1decce
Victor Declerk <victor.declerk@gmail.com>
2021-07-31 09:36:51 -0300

    Moved repositories created in test folder in error.

--- src/main/java/com/v15k/springbootreloaded/SpringBootReloadedApplication.java
+++ src/main/java/com/v15k/springbootreloaded/SpringBootReloadedApplication.java
@@ -2,11 +2,12 @@ package com.v15k.springbootreloaded;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-public class SpringBootReloadedApplication {
+public class SpringBootReloadedApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReloadedApplication.class, args);
